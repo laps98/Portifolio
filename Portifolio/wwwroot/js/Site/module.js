36ac35ba8702a2ad2ec2f3978782cf5dd3b13336
laps98 <pine998@hotmail.com>
2024-07-31 08:25:10 -0300

    angular

---
+++ Portifolio/wwwroot/js/Site/module.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var app = angular.module("app");
+
+    app.controller('SiteController', function($scope){
+
+        $scope.message = "Hello from AngularJS!";
+
+    });
+
+})();
