cb074a9f851e6e3563bc36a97a4ff4629d80222a
laps98 <pine998@hotmail.com>
2024-08-01 08:15:27 -0300

    .

--- Portifolio/wwwroot/js/Site/module.js
+++ Portifolio/wwwroot/js/Site/module.js
@@ -1,7 +1,28 @@
 ﻿(function () {
-    var app = angular.module('app', []);
+    var app = angular.module('app');
 
-    app.controller('SiteController', function($scope){
+    app.service('$dataService', function ($http) {
+        this.getData = function (cep) {
+            return $http.get(`https://viacep.com.br/ws/${cep}/json/`);
+        };
+    });
+
+    app.controller('SiteController', function ($scope, $dataService) {
+
+        $scope.endereco = [];
+
+        $scope.buscarCep = () => {
+            if ($scope.endereco.Cep == null || $scope.endereco.Cep.length != 8)
+                return;
+            $dataService.getData($scope.endereco.Cep).then(function (response) {
+                let data = response.data;
+                $scope.endereco.Bairro = data.bairro;
+                $scope.endereco.Localidade = data.localidade;
+                $scope.endereco.Logradouro = data.logradouro;
+                $scope.endereco.Uf = data.uf;
+
+            });
+        }
 
         $scope.message = "Hello from AngularJS!";
 
