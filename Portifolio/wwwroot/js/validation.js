35a08adf085cfdc04aa7ff43accd3cfa93387536
laps98 <pine998@hotmail.com>
2024-08-26 08:46:52 -0300

    .

---
+++ Portifolio/wwwroot/js/validation.js
@@ -0,0 +1,20 @@
+﻿$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element) {
+        $(element).removeClass('is-invalid');
+    },
+    ignore: ":hidden:not(.boolean-validation)",
+    errorElement: 'span',
+    errorClass: 'invalid-tooltip',
+    errorPlacement: function (error, element) {
+        element.parent().addClass('position-relative');
+
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
